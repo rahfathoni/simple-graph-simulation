f95e9708ea3b41157250c9b473e9dfa66b611c61
rahfathoni <rahfathoni@gmail.com>
2023-02-15 19:58:05 +0700

    tamnbah service

--- server/controllers/FoodController.js
+++ server/controllers/FoodController.js
@@ -1,13 +1,19 @@
 const { Food } = require("../models/index");
 
+const formatingDate = (item) => {
+  const date = new Date(item);
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+};
+
 class FoodController {
   // router.post("/", FoodController.addFood);
   static addFood(req, res, next) {
-    const { name, amount, price } = req.body;
+    const { name, amount, price, createdAt } = req.body;
     const input = {
       name: name ? name.toUpperCase() : name,
       amount,
       price,
+      createdAt: createdAt ? new Date(createdAt) : new Date(),
     };
     Food.create(input)
       .then((data) => {
@@ -32,8 +38,123 @@ class FoodController {
     };
     Food.findAll(options)
       .then((data) => {
+        if (!data || data.length === 0) {
+          return res.status(200).json({
+            list: [],
+          });
+        }
+        const newData = data.map((x) => ({
+          name: x.name,
+          amount: x.amount,
+          price: x.price,
+          createdAt: x.createdAt,
+          date: formatingDate(x.createdAt),
+        }));
         res.status(200).json({
-          list: data,
+          list: newData,
+        });
+      })
+      .catch((err) => {
+        return next(err);
+      });
+  }
+
+  // router.get("/search", FoodController.readFoodByName);
+  static readFoodByName(req, res, next) {
+    const { name } = req.body;
+    const options = {
+      where: {
+        name: name ? name.toUpperCase() : name,
+      },
+      order: [["createdAt", "ASC"]],
+    };
+    Food.findAll(options)
+      .then((data) => {
+        if (!data || data.length === 0) {
+          return res.status(200).json({
+            name: "",
+            list: [],
+          });
+        }
+        const newData = data.map((x) => ({
+          name: x.name,
+          amount: x.amount,
+          price: x.price,
+          createdAt: x.createdAt,
+          date: formatingDate(x.createdAt),
+        }));
+        const mapping = newData.reduce((map, item) => {
+          let date = item.date;
+          let amount = +item.amount;
+          map[date] = (map[date] || 0) + amount;
+          return map;
+        }, {});
+        const result = Object.keys(mapping).map((date) => {
+          return {
+            date: date,
+            totalAmount: mapping[date],
+          };
+        });
+        res.status(200).json({
+          name: data[0].name,
+          list: result,
+        });
+      })
+      .catch((err) => {
+        return next(err);
+      });
+  }
+
+  // router.get("/list", FoodController.readList);
+  static readList(req, res, next) {
+    const options = {
+      attributes: { include: ["name"] },
+      order: [["createdAt", "ASC"]],
+    };
+    Food.findAll(options)
+      .then((data) => {
+        if (!data || data.length === 0) {
+          return res.status(200).json({
+            list: [],
+          });
+        }
+        const uniqueName = [...new Set(data.map((item) => item.name))];
+        res.status(200).json({
+          list: uniqueName,
+        });
+      })
+      .catch((err) => {
+        return next(err);
+      });
+  }
+
+  // router.get("/total", FoodController.readTotal);
+  static readTotal(req, res, next) {
+    const options = {
+      attributes: { include: ["name"] },
+      order: [["createdAt", "ASC"]],
+    };
+    Food.findAll(options)
+      .then((data) => {
+        if (!data || data.length === 0) {
+          return res.status(200).json({
+            list: [],
+          });
+        }
+        const mapping = data.reduce((map, item) => {
+          let name = item.name;
+          let amount = +item.amount;
+          map[name] = (map[name] || 0) + amount;
+          return map;
+        }, {});
+        const newData = Object.keys(mapping).map((name) => {
+          return {
+            food: name,
+            totalAmount: mapping[name],
+          };
+        });
+        res.status(200).json({
+          list: newData,
         });
       })
       .catch((err) => {
--- server/routes/foodRouter.js
+++ server/routes/foodRouter.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const FoodController = require("../controllers/FoodController");
 
 router.post("/", FoodController.addFood);
-router.get("/", FoodController.readFood);
 router.delete("/:foodId", FoodController.deleteFood);
+router.get("/", FoodController.readFood);
+router.get("/search", FoodController.readFoodByName);
+router.get("/list", FoodController.readList);
+router.get("/total", FoodController.readTotal);
 
 module.exports = router;
